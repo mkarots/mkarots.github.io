db817c9a2091ee113ea8aaee5cfbd50b1a378841
Michael Karotsieris <michael.karotsieris@gmail.com>
2025-12-28 14:32:56 +0000

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mkarots.github.io',
-  base: '/blogfolio/',
   integrations: [mdx(), sitemap(), react()],
   markdown: {
     shikiConfig: {
